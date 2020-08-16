4a3829f0606deed436a7c5ee7eed06db2ad4238f
empjosh <empjoshkc@gmail.com>
2020-08-16 17:54:51 +0000

    Stoned is a file that has some of the main changes

---
+++ newreact-rutorial/src/Stoned.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Stoned extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Ich habe gute news. The flower just DIED!!! MUHU HAHAHAHAHAHAH</h1>
+        <h2>Du weis was wunderschoonist</h2>
+        <h3>Oorder? Liise das estre wort noch mal</h3>
+        <h4>Nein, nein, ist guute</h4>
+        <h5>Gutentag!!!</h5>
+        <h6>Panzersechwerzasteacasican</h6>
+      </div>
+    );
+  }
+}
+
+export default Stoned;
